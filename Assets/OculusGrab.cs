d5e1590cb304b266a19d95d0e40d7dfc9de49890
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-03-05 13:26:23 -0800

    Initial commit

---
+++ Assets/OculusGrab.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OculusGrab : MonoBehaviour
+{
+    public GameObject CollidingObject;
+    public GameObject objectInHand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
