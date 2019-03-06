a480bb46887dcb9e15442b6d63031229445749f6
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-03-06 13:40:16 -0800

    Removed head bobbing and changed movement to FixedUpdate

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -14,7 +14,7 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
         float lTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
         bool startedWalking = OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger);
         bool stoppedWalking = OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger);
@@ -31,10 +31,12 @@ public class Movement : MonoBehaviour
         if (lTrigger > 0) {
             Vector3 direction = leftController.transform.forward;
             direction.y = 0;
-            this.transform.Translate(direction * lTrigger * 0.5f);
-            Vector3 pos = this.transform.position;
-            pos.y = Mathf.Sin((Time.time - timeStartedWalking)*5*lTrigger) * 0.1f;
-            this.transform.position = pos;
+            this.transform.Translate(direction * lTrigger * 0.3f);
+            
+            //Head Bobbing
+            //Vector3 pos = this.transform.position;
+            //pos.y = Mathf.Sin((Time.time - timeStartedWalking)*5*lTrigger) * 0.1f;
+            //this.transform.position = pos;
         }
     }
 }
