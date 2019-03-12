7fea56e9d0379caf29dff0d8ca77eb3a7c565e39
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-03-11 17:06:46 -0700

    Fine tuning for grabbing objects

--- Assets/GrabToyParts.cs
+++ Assets/GrabToyParts.cs
@@ -16,14 +16,14 @@ public class GrabToyParts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, rightHand.transform.position) < 1) {
+        if (Vector3.Distance(transform.position, rightHand.transform.position) < 0.4) {
             bool rightHandGrab = OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger);
             if (rightHandGrab) {
                 GameObject.Instantiate(grababbleObj);
             }
         }
 
-        if (Vector3.Distance(transform.position, leftHand.transform.position) < 1) {
+        if (Vector3.Distance(transform.position, leftHand.transform.position) < 0.4) {
             bool leftHandGrab = OVRInput.GetDown(OVRInput.Button.PrimaryHandTrigger);
             if (leftHandGrab) {
                 GameObject.Instantiate(grababbleObj);
