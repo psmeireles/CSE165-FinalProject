a803401c464221a08c5cff4ac0b30ccc8ae69a38
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-03-12 18:16:36 -0700

    Added toy coloring

---
+++ Assets/ToyColorizer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyColorizer : MonoBehaviour
+{
+    int currentPart;
+    MeshRenderer[] parts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parts = this.gameObject.GetComponentsInChildren<MeshRenderer>();
+        currentPart = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Bullet") {
+            parts[currentPart].material = collision.gameObject.GetComponent<MeshRenderer>().material;
+            currentPart = (currentPart + 1) % parts.Length;
+        }
+    }
+}
