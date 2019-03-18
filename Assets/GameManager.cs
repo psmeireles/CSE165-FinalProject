3e200b1ebdb9e8a4070f4088382ce9ec805be9d9
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-03-18 14:45:53 -0700

    Added end game logic

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,15 +6,26 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public Text hudText;
+    public Text timer;
+    float startTime;
+    bool finished;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startTime = Time.time;
+        finished = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        hudText.text = "Remaining tables: " + FinishedToysTable.remainingTables.ToString();
+        if(FinishedToysTable.remainingTables != 0 && ! finished) {
+            hudText.text = "Remaining tables: " + FinishedToysTable.remainingTables.ToString();
+            timer.text = "Elapsed time: " + (Time.time - startTime).ToString("0.00");
+        }
+        else {
+            hudText.text = "Finished!";
+            finished = true;
+        }
     }
 }
